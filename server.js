c38a0de9135c541f84efb3c75958a1c729d9e5ed
David Agustin <davidsyagustin@gmail.com>
2019-02-22 16:15:17 -0800

    Update server.js to be compatible with Heroku

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const port = process.env.PORT || 4000;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = Number(process.env.PORT || 4000);
+server.use(middlewares);
+server.use(router);
+server.listen(port, function () {
+  console.log('JSON Server is running')
+});
